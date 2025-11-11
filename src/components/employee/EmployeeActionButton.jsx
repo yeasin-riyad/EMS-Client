676c98132b82f81ec893c6bd904d5a00f58c27bb
Yeasin-Riyad <mazumderyeasin98@gmail.com>
2025-11-11 17:54:47 +0600

    changes modal

--- src/components/employee/EmployeeActionButton.jsx
+++ src/components/employee/EmployeeActionButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
-import ViewEmployeeModal from './viewEmployeeModal'
+import ViewEmployeeModal from './ViewEmployeeModal'
 import { useSelector } from 'react-redux'
 import { useEffect } from 'react'
 import EditEmployeeModal from './EditEmployeeModal'
